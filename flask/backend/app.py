931fb0b0a9e3862328ca86690db63e7d5d68c657
amirizzudin4 <amirizzudin4@gmail.com>
2018-12-07 15:47:41 +0800

    done on scrap api

--- flask/backend/app.py
+++ flask/backend/app.py
@@ -1,15 +1,56 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import model
+from datetime import datetime
+from datetime import timedelta
+import requests
+import urllib
+import lxml
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+
+# from plotly import py
+# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# import plotly.graph_objs as go
+# init_notebook_mode(connected=True)
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def return_hello():
-    return 'hello from flask docker conatiner '
-
-
-@app.route('/hello')
 def return_another():
     return 'lol hohoho'
+
+
+@app.route('/api', methods=['POST', 'GET'])
+def original_data():
+    symb = ''
+    if request.method == 'POST':
+        if request.accept_mimetypes.accept_json:
+            symbpost = request.get_json(force=True)
+            print(symbpost.symbol)
+        else:
+            symbpost = request.form['symbol']
+
+        url = 'https://finance.yahoo.com/quote/'+symbpost+'/history?p='+symbpost
+    else:
+        symb = request.args.get('symbol')
+        url = 'https://finance.yahoo.com/quote/'+symb+'/history?p='+symb
+
+    requesting = requests.get(url)
+    soup = BeautifulSoup(requesting.text, 'html.parser')
+    table_big = soup.find('table', attrs={'data-test': 'historical-prices'})
+    soup.find('tfoot').decompose()
+
+    missing_values = ["n/a", "na", "-", "NaN"]
+    rs = pd.read_html(str(table_big), na_values=missing_values)
+    rs = rs[0].to_json(orient='records')
+
+    return rs
 
 
 if __name__ == '__main__':
--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -1,25 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {} from "react-bootstrap";
+import loading from "./asset/img/loading.gif";
+import axios from "axios";
+
+const loadstyle = {
+  textAlign: "center"
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputted: 0, inputfield: null };
+
+    this.submit = this.submit.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
+  }
+
+  submit() {
+    this.setState({ inputted: 1 });
+
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api",
+      data: {
+        symbol: this.state.inputfield
+      }
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  updateInputValue(evt) {
+    //console.log("input field updated with "+evt.target.value);
+    this.setState({ inputfield: evt.target.value });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="centerbox">
+        <div className="formbox">
+          {/* {this.state.updateInputValue} */}
+          {this.state.inputted === 1 && (
+            <div style={loadstyle}>
+              <img src={loading} alt="loading" />
+              <h1>Preparing your prediction</h1>
+            </div>
+          )}
+          {this.state.inputted === 2 && (
+            <div>
+              <h1>siap</h1>
+            </div>
+          )}
+          {this.state.inputted === 0 && (
+            <form id="symbolform">
+              <input
+                type="text"
+                className="form-control"
+                id="usr"
+                onChange={this.updateInputValue}
+                placeholder="input your symbol here"
+              />
+              <br />
+              <button
+                type="button"
+                className="btn btn-primary btn-block"
+                onClick={this.submit}
+              >
+                Submit
+              </button>
+            </form>
+          )}
+        </div>
       </div>
     );
   }
